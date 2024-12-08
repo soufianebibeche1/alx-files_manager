6f788995f7573d5362e7f59ace386e7a02feee34
soufianebibeche1 <soufiane.bibeche1@gmail.com>
2024-12-08 13:34:49 -0800

    testem

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const routes = require('./routes/index')
+const express = require('express');
+const routes = require('./routes/index');
 
+const app = express();
+const host = 'localhost';
+const port = process.env.PORT || 5000;
+app.use('/', routes);
 
-const app = express()
-const host = 'localhost'
-const port = process.env.PORT || 5000
-app.use('/', routes)
-
-app.listen(port, host, ()=> {
-    console.log(`Server running on port ${port}`)
-})
-module.exports = app
+app.listen(port, host, () => {
+  console.log(`Server running on port ${port}`);
+});
+module.exports = app;
