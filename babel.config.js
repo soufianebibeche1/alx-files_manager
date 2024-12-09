6e6df46f462dbf59a7cd11da73aa7d63b4195ce1
soufianebibeche1 <soufiane.bibeche1@gmail.com>
2024-12-09 14:39:17 -0800

    add

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
         },
-      ],
+      },
     ],
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.json'],
+      },
+    ],
+  ],
 };
